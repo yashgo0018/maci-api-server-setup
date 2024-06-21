a7217fe477a41a2c8acc0b5fe29964091da00e0d
Yash Goyal <yashgo0018@gmail.com>
2024-06-21 19:28:54 +0100

    fixed a bug

--- refresh.sh
+++ refresh.sh
@@ -4,5 +4,4 @@ export CERTBOT_COMMAND_FLAG=force-renewal
 docker compose pull
 docker compose down
 docker compose rm -f
-docker volume rm -f $(docker volume ls -q | grep frontend)
 docker compose up -d
